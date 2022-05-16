6c64b314f2ba430ef102c03af62437b59c1a74d5
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-05-16 19:58:02 +0300

    Initial Commit

---
+++ Balance App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Balance App
+//
+//  Created by Олейник Богдан on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
